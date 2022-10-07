e483fbea86dc54714d74bb858f9bd44f37c9d701
Dscdago <daguito_saenz@hotmail.com>
2022-10-06 23:14:31 -0600

    Added entry_id to database. Show person's entry #

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from cs50 import SQL
 from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 
@@ -140,9 +139,13 @@ def entry():
 
         customer_id = session["user_id"]
 
+        # Check length of user's entries
+        entryNum = len(db.execute("SELECT * FROM entries WHERE customer_id == ?", customer_id))
+        entryNum += 1
+
         # add entry into entries database
-        db.execute("INSERT INTO entries (customer_id, date, year, month, day, entry) VALUES(?, ?, ?, ?, ?, ?)",
-                    session["user_id"], date, year, month, day, entry)
+        db.execute("INSERT INTO entries (customer_id, entry_id, date, year, month, day, entry) VALUES(?, ?, ?, ?, ?, ?, ?)",
+                    session["user_id"],entryNum, date, year, month, day, entry)
 
         # Once entry is submitted. Redirect user to main page
         return redirect("/")
